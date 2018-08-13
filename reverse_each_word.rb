e79e3ecb9e30e7d6509edc06e7eddbbd50031ac8
Ricky Liu <rorix12321@gmail.com>
2018-08-13 05:01:53 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(sentence)
-  sentence2 = []
-  sentence.each do |x|
-    sentence << x.reverse
+  sentence2 = "#{sentence}"
+  sentence3 = []
+  sentence2.each do |x|
+    sentence3 << x.reverse
   end
-  return sentence
+  return sentence3
 end
