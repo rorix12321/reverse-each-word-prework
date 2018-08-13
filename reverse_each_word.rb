0052ca5fcb814d94eba6aed7f73ed55e23c14e49
Ricky Liu <rorix12321@gmail.com>
2018-08-13 05:24:01 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,10 +7,3 @@ def reverse_each_word(sentence)
   newsentence = emptyarray.join(" ")
   return newsentence
 end
-
-def reverse_each_word(sentence)
-  sentence.collect do |x|
-    x.reverse
-  end
-  return reverse_each_word
-end
